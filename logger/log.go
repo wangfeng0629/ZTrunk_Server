100d044df5206592ad0be44423861abc98a1819c
beyondhxl <459898432@qq.com>
2019-01-15 17:00:45 +0800

    [日志系统]增加一处注释

--- logger/log.go
+++ logger/log.go
@@ -7,6 +7,7 @@ type Log interface {
 	// 设置日志级别
 	SetLevel(level int)
 
+	// 各级别日志
 	Debug(format string, args ...interface{})
 	Trace(format string, args ...interface{})
 	Info(format string, args ...interface{})
